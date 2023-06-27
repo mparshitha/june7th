68612f34df9a09d16ea2b9348317ba68e7ddcd63
mparshitha <arshithamp.xworkz@gmail.com>
2023-06-27 14:43:20 +0530

    This is my first commit

---
+++ Bike.java
@@ -0,0 +1,10 @@
+class Bike{
+ public static void main(String[] args){
+	System.out.println("He goton his purple bike and left");
+	System.out.println("I borrowed her bike from her for a day");
+	System.out.println("Bike is used for racing and sport");
+	System.out.println("Bikes help us navigate distances quickly");
+	System.out.println("Bike is also known as motorcycle");
+	
+}
+}
