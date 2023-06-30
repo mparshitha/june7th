fd2ac5d2c206fe84c2840d98f469f30cbfaa3e1a
mparshitha <arshithamp.xworkz@gmail.com>
2023-06-30 13:43:40 +0530

    trying folder

---
+++ Test/Test1.java
@@ -0,0 +1,75 @@
+1.b)31
+2.c)20
+3.c)96
+4.a)60
+5.a)9
+6.b)K
+7.a)P
+8.b)FR
+9.c)bccab
+10.a)GOV
+11.2
+12.38
+13.Window
+14.Sun
+15.BE
+                                                                 
+																 
+																 
+																 
+																 
+															                                                 Java TEST
+																
+																
+1.Java is a source file where a program will be stored in .java, whereas javac is compiler where we compile the java program.
+
+2.JDK-its is a java development kit where we develop a java program, JRE-it ia called as java runtine environment,it means in environmental vurtuals where we creat a path for the program that is java.HOME,JVM- it is called as java virtual machine means we cannot able to see the class of the program it is virtual.
+
+4.Class- it is datatype or non primitive or class
+  Object-where we use statement with a refrence variable declaration properties and print the message.
+  Signature if the class-
+
+5.Array is used to store and retriev the reference value.
+
+6.java file can be run in JDK and JRE whereas Class file can be run in JVM.
+
+7.6
+
+9.Java is a platform independence because of its byte code and it is object oriented programming language where we have methods,class,functionalities and instances.
+
+10.Variables are  used to stored the data values.
+   different types of variables are:
+   1.Refrence variable- these are the variables where they point the address of the initialised values in constant pool.
+   2.Local variables-these are variable which can return in parameters using paranthisies.
+
+11.Class variable is a non primitive class or a data type.
+
+12.Data types are the non primitive class files.
+   types of data types are:
+   1.Primitive Datatype-These are byte,short,int,long,float,double,char,boolean. which are used for single number,decimal values,single characters and true or false conditions.
+   2.Non primitive Data type-these are implicit or explicit values of class datatype.
+
+13.*Java is platform independence beacuse of its byte code.
+   *java is a object oriented language.
+   *in java program java file can be run in JDK and JRE.
+   *In java program class file can be run in JVM.
+   *Java is consists of both primitive as well as non primitive datatypes
+
+16.It is a statement where System is known as pre-defined class name,out is static variable, println is the static method
+
+17.It is used to point the same memory location address of 2 different reference variable in constant pool. from the stack.
+
+18.It is used to access refrence variable values.
+
+19.object class or class Class_name{} 
+
+20.*char. 
+   *byte.
+   *new.
+   *return. 
+   *if.
+   *else.
+   *static.
+   *public.
+   *void.
+   *main.
