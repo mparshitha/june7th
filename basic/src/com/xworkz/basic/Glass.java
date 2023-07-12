d6b38e22c92deb0e1e96daa8662d5958d71cae33
mparshitha <mparshitha.xworkz@gmail.com>
2023-07-12 12:21:44 +0530

    project1

---
+++ basic/src/com/xworkz/basic/Glass.java
@@ -0,0 +1,10 @@
+package com.xworkz.basic;
+
+public class Glass {
+
+	public static void main(String[] args) {
+		System.out.println("Running Glass");
+
+	}
+
+}
--- project1/src/xworkz/Bottle.java
+++
@@ -1,9 +0,0 @@
-package xworkz;
-
-public class Bottle {
-
-	public static void main(String[] args) {
-		System.out.println("Running Bottle");
-	}
-
-}
